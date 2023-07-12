9c5c31911a0e7bfe0a063428bae3f426179a4326
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2023-07-12 09:39:31 +0000

    docs: add desciptions to tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "lambda_fn_arn" {
-  value = local.enabled ? aws_lambda_function.this[0].arn : ""
+  value       = local.enabled ? aws_lambda_function.this[0].arn : ""
+  description = "The ARN of the AWS Lambda function running the Yopass server."
 }
 
 output "lambda_fn_execution_url" {
-  value = local.enabled ? aws_lambda_function_url.this[0].function_url : ""
+  value       = local.enabled ? aws_lambda_function_url.this[0].function_url : ""
+  description = "	The URL for invoking the AWS Lambda function running the Yopass server."
 }
