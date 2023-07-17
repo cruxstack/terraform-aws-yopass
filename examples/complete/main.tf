a16df4eb6edff9e0b58faed21fcba0c29f1d7691
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2023-07-17 16:02:51 +0000

    ci: fix all issues with complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,10 +8,8 @@ locals {
 module "yopass" {
   source = "../../"
 
-  website_domain_name     = local.website_domain_name
+  website_domain_name     = local.domain_name
   website_certificate_arn = module.ssl_certificate.arn
-
-  context = module.this.context
 }
 
 # ---------------------------------------------------------------------- dns ---
@@ -20,7 +18,7 @@ module "dns" {
   source  = "cloudposse/route53-alias/aws"
   version = "0.13.0"
 
-  aliases         = [local.website_domain_name]
+  aliases         = [local.domain_name]
   parent_zone_id  = local.domain_parent_hosted_zone_id
   target_dns_name = module.yopass.website_cloudfront_domain_name
   target_zone_id  = module.yopass.website_cloudfront_hosted_zone_id
@@ -33,7 +31,7 @@ module "ssl_certificate" {
   source  = "cloudposse/acm-request-certificate/aws"
   version = "0.17.0"
 
-  domain_name                       = local.website_domain_name
+  domain_name                       = local.domain_name
   process_domain_validation_options = true
   ttl                               = "60"
   zone_id                           = local.domain_parent_hosted_zone_id
--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,3 +1,8 @@
+output "website_endpoint" {
+  value       = "https://${local.domain_name}"
+  description = "Address of the website_endpoint"
+}
+
 output "server_api_endpoint" {
   value       = module.yopass.server_apigw_url
   description = "The API endpoint URL of the Yopass server"
